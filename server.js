c237f7afeeed9325f921968aec146a31ee19a777
Jesus Esquer <jme2791@gmail.com>
2019-12-07 19:18:13 -0700

    Updated Mongodb connection to heroku

--- server.js
+++ server.js
@@ -26,8 +26,12 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/unit18Populater";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
 
 // Routes
 
@@ -143,9 +147,8 @@ app.get("/savedArticles", function(req, res) {
 app.get("/deleteArticle/:id", function(req, res) {
   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
   db.SavedArticle.remove({ _id: req.params.id })
-    // ..and populate all of the notes associated with it
+    // ..and Remove all of the notes associated with it
     .then(function(dbArticle) {
-      // If we were able to successfully find an Article with the given id, send it back to the client
       res.json(dbArticle);
     })
     .catch(function(err) {
